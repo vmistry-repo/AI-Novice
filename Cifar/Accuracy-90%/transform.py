49104b2b322582a3dd36508cd7d44206baa86509
vmistry <mistryvishal1994@gmail.com>
2023-07-12 12:26:01 +0530

    Update transform.py

--- Cifar/Accuracy-90%/transform.py
+++ Cifar/Accuracy-90%/transform.py
@@ -9,17 +9,6 @@ from albumentations.pytorch import ToTensorV2
 cv2.setNumThreads(0)
 cv2.ocl.setUseOpenCL(False)
 
-class Cifar10SearchDataset(torchvision.datasets.CIFAR10):
-    def __init__(self, root="~/data/cifar10", train=True, download=True, transform=None):
-        super().__init__(root=root, train=train, download=download, transform=transform)
-
-    def __getitem__(self, index):
-        image, label = self.data[index], self.targets[index]
-        if self.transform is not None:
-            transformed = self.transform(image=image)
-            image = transformed["image"]
-        return image, label
-
 def get_train_transforms(pixel_means, pixel_stds):
   train_transforms = A.Compose(
       [
