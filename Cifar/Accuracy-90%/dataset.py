0aa9b0f5b64fd52644692a3e8a7a4ac40775429b
Vishal <mistryvishal1994@gmail.com>
2023-07-12 12:04:30 +0530

    Cifar-With-OneCycleLR

---
+++ Cifar/Accuracy-90%/dataset.py
@@ -0,0 +1,12 @@
+import torchvision
+
+class Cifar10SearchDataset(torchvision.datasets.CIFAR10):
+    def __init__(self, root="~/data/cifar10", train=True, download=True, transform=None):
+        super().__init__(root=root, train=train, download=download, transform=transform)
+
+    def __getitem__(self, index):
+        image, label = self.data[index], self.targets[index]
+        if self.transform is not None:
+            transformed = self.transform(image=image)
+            image = transformed["image"]
+        return image, label
